ea2b56107e8956c9ea7803cb685f867a3f37421c
Milan Panchal <sam07it22@gmail.com>
2020-12-19 17:03:39 +0530

    Initial Commit

---
+++ DetectScreenCaptureDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetectScreenCaptureDemo
+//
+//  Created by Milan Panchal on 19/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
